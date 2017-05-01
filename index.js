e9a5ad91ef61f5f88690558cccc68ac1a50f66f0
tomymolina <tm07081995@gmail.com>
2017-05-01 17:30:35 +0200

    Implementar versión funcional

--- index.js
+++ index.js
@@ -2,49 +2,54 @@
 
 const app = require('express')()
 const {inspect} = require('util')
+const cheerio = require('cheerio')
+const axios = require('axios')
 
-const PORT = 4000
+const PORT = 4001
 
 app.use(require('body-parser').json())
 
 app.use(require('morgan')('combined'))
 
-const sentences = [
-    'Un día de estos 😁',
-    'Pues no sé chico 😔',
-    'Pregúntaselo a él 😒',
-]
-
-const sentencesAfterResponse = [
-    '¿Sabías que Hitler murió el 30 de Abril de 1945?',
-    'Un dato interesante: el FCB se fundó el 29 de noviembre de 1899',
-    'Ahí va un dato: Tokio es una de las ciudades más pobladas del mundo con 35 millones de habitantes en su zona metropolitana'
-]
-
-function randomSentence() {
-    return sentences[Math.floor(Math.random() * sentences.length)]
+function getChiste(html, indice) {
+    const $ = cheerio.load(html)
+    const eq = indice + 3
+    return $('#wrapper > div').eq(12).find('p').eq(0).text().trim()
 }
 
-function randomFactSentence() {
-    return sentencesAfterResponse[Math.floor(Math.random() * sentences.length)]
+function getMejorChiste(nPagina, indice){
+    const url = 'http://www.chistescortos.eu/top?page=' + (nPagina + 1)
+
+    return axios.get(url)
+        .then(response => {
+            if (response.data) {
+                return getChiste(response.data, indice)
+            } else {
+                return Promise.reject()
+            }
+        })
 }
 
 app.post('/',
     (req, res, next) => {
         console.log('new request', inspect(req.body, {depth: 10}))
 
-        if (req.body.conversation.messages.length > 1) {
-            res.json({
-                type: 3,
-                text: `${randomFactSentence()}, ¿te gustaría saber otro dato curioso 😋?`
+        const indice = Math.floor(Math.random() * 10)
+        const pagina = Math.floor(Math.random() * 40)
+
+        console.log(indice, pagina)
+
+        getMejorChiste(indice, pagina)
+            .then(chiste => {
+                res.json({
+                    text: chiste,
+                    type: 1
+                })
             })
-        } else {
-            res.json({
-                type: 3,
-                text: `${randomSentence()}, pero podemos seguir hablando si quieres 😉`
+            .catch(err => {
+                console.error('error', err)
+                res.sendStatus(500)
             })
-        }
-        
     }
 )
 
